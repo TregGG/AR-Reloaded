b8064b6026a3bd85ac0e376c21ac1fc2fff5885e
Hakaluka <prsaryanjain@gmail.com>
2026-04-18 05:28:27 +0530

    added a back button from Ar screen

--- Assets/scripts/ui.cs
+++ Assets/scripts/ui.cs
@@ -8,4 +8,9 @@ public class ui : MonoBehaviour
         // 3. Get the current build index and load index + 1
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void MainMnueScene()
+    {
+        // 3. Get the current build index and load index + 1
+        SceneManager.LoadScene(0);
+    }
 }
